43c4570e74e9559c5a31d7ca0f3809ae46753dfd
Avon46 <142923631+Avon46@users.noreply.github.com>
2026-05-21 15:03:46 +0800

    add BaseSection

---
+++ src/components/BaseSection.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="base-section">
+        <div class="base-section__title">
+            <slot name="title"></slot>
+        </div>
+
+        <div class="base-section__content">
+            <slot></slot>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.base-section {
+    border: 1px solid #ddd;
+    border-radius: 12px;
+    padding: 20px;
+    margin-bottom: 20px;
+    background-color: #fff;
+}
+
+.base-section__title {
+    margin-bottom: 16px;
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.base-section__content {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+}
+</style>
