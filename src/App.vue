9b05303ea8196ecda846438e2bbeb4164a6b49b1
Avon46 <142923631+Avon46@users.noreply.github.com>
2026-05-21 09:57:31 +0800

    feature: add LoginSimple and App.vue-emit

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import PolicySimple from './views/PolicySimple.vue'
-const loading = ref(false)
-const username = ref('')
-const password = ref('')
-const message = ref('')
+import LoginSimple from './views/LoginSimple.vue'
 
-function login() {
-  loading.value = true
-  message.value = ''
-  if (!username.value || !password.value) {
-    message.value = '請輸入帳號密碼'
-    loading.value = false
-    return
-  }
-  if (username.value === 'admin' && password.value === '123456') {
-    message.value = '登入成功'
-  } else {
-    message.value = '帳號或密碼錯誤'
-  }
-  loading.value = false
+const isLoggedIn = ref(false)
+const loginUser = ref('')
+
+function handleLoginSuccess(username: string) {
+  isLoggedIn.value = true
+  loginUser.value = username
 }
+
+
 </script>
 
 <template>
-  <h2>登入系統</h2>
-  <p><input v-model.trim="username" placeholder="帳號"></p>
-  <input v-model.trim="password" type="password" placeholder="密碼">
-  <p><button @click="login">登入</button></p>
-  <p v-if="loading">登入中</p>
-  <p v-else-if="message">{{ message }}</p>
-  <PolicySimple></PolicySimple>
+  <LoginSimple @login-success="handleLoginSuccess"></LoginSimple>
+  <p>目前登入狀態：{{ isLoggedIn }}</p>
+  <p v-if="isLoggedIn">目前登入者:{{ loginUser }}</p>
+  <PolicySimple v-if="isLoggedIn"></PolicySimple>
 </template>
---
+++ src/views/LoginSimple.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const loading = ref(false)
+const username = ref('')
+const password = ref('')
+const message = ref('')
+const emit = defineEmits<{
+    'login-success': [username: string]
+}>()
+
+function login() {
+    loading.value = true
+    message.value = ''
+    if (!username.value || !password.value) {
+        message.value = '請輸入帳號密碼'
+        loading.value = false
+        return
+    }
+    if (username.value === 'admin' && password.value === '123456') {
+        message.value = '登入成功'
+        emit('login-success', username.value)
+    } else {
+        message.value = '帳號或密碼錯誤'
+    }
+    loading.value = false
+}
+
+
+</script>
+
+<template>
+    <h2>登入系統</h2>
+    <p><input v-model.trim="username" placeholder="帳號"></p>
+    <input v-model.trim="password" type="password" placeholder="密碼">
+    <p><button @click="login">登入</button></p>
+    <p v-if="loading">登入中</p>
+    <p v-else-if="message">{{ message }}</p>
+</template>
